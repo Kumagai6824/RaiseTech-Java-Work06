6f98ab0fe0f015d97cc3d38f136b308f463fe268
Kumagai-java2211 <j.min.howsand682@gmail.com>
2022-12-18 11:27:14 +0900

    [Add]helloWorldの実装環境構築

---
+++ src/main/java/com/RaiseTech/Work06/Work06Application.java
@@ -0,0 +1,13 @@
+package com.RaiseTech.Work06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work06Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/RaiseTech/Work06/Work06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RaiseTech.Work06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
