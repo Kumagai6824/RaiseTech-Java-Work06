c5c4ecf1c7c6c96d4d14d906fb5453e602e967fe
Kumagai-java2211 <j.min.howsand682@gmail.com>
2022-12-18 11:39:25 +0900

    [Add]helloWorldの実装

---
+++ src/main/java/com/RaiseTech/Work06/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.RaiseTech.Work06;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public Map<String ,String> hello(){
+        return Map.of("message","hello world");
+    }
+}
